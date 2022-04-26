ad2da6ff760e583cd8fa679207561b18485daed4
Gal Halifa <halifag@bgu.ac.il>
2022-04-26 20:26:10 +0300

    UserServiceTest BoardServiceTest

---
+++ BackendTests/BoardServiceTest.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Globalization;
+
+class BoardServiceTest
+{
+    public BoardServiceTest()
+    {
+
+    }
+
+    DateTime dateTime = new DateTime;
+
+    public bool addTask(string title, string description)
+    {
+
+    }
+
+    public bool removeTask(string title)
+    {
+
+    }
+
+    public bool editTask(string oldTitle, string newTitle)
+    {
+
+    }
+
+    public bool editTaskDescription(string title, string newDescription)
+    {
+
+    }
+
+    public bool editTaskDueDate(string title, DateTime newDueDate)
+    {
+
+    }
+
+    public bool remobeBoard(string name)
+    {
+
+    }
+
+    public bool advanceTaskPhase(string title)
+    {
+
+    }
+
+
+
+    public string runTest()
+    {
+
+    }
+}
---
+++ BackendTests/UserServiceTest.cs
@@ -0,0 +1,26 @@
+using System;
+
+
+class UserServiceTest
+{
+   public UserServiceTest()
+    {
+       
+    }
+    
+    public bool RegisterTest(string email, string password){
+    }
+   
+    public bool Login(string email, string password){
+
+    }
+
+    public bool LogOut(string email){ 
+    }
+
+
+    public string runTest()
+    {
+        
+    }
+}
