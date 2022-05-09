404fe3926c75de86e6d3f481d05958d4e6ac288f
Tomer Naydnov <tomernay@post.bgu.ac.il>
2022-05-09 16:05:50 +0300

    Implemented Column class on the Business Layer

--- Backend/BusinessLayer/Column.cs
+++ Backend/BusinessLayer/Column.cs
@@ -9,41 +9,60 @@ namespace IntroSE.Kanban.Backend.BusinessLayer
     internal class Column
     {
         private string name { get; set; }
-        public int maxTasks { get; set; }
+        private int maxTasks { get; set; }
         private List<Task> tasks { get; set; }
 
         public Column(string name)
         {
-            throw new NotImplementedException();
+            this.name = name;
+            tasks = new List<Task>();
+            maxTasks = -1; //The value is -1 if there isn't a limit on the max tasks possible. Any number higher than 0 is the limit number.
         }
 
-        public void addTask(string boardName, string title, string description)
+        public void addTask(string title, string description, DateTime dueDate)
         {
-            throw new NotImplementedException();
+            if (maxTasks != -1 & tasks.Count == maxTasks)
+            {
+                throw new Exception("The maximum capacity of tasks in this column is full. It is not possible to add another task");
+            }
+            Task newTask = new Task(title, description, dueDate);
+            tasks.Add(newTask);
         }
 
-        public Boolean removeTask(string boardName, string title)
+        public Boolean removeTask(Task task)
         {
-            throw new NotImplementedException();
+            if (!tasks.Contains(task))
+            {
+                return false;
+            }
+            tasks.Remove(task);
+            return true;
         }
 
-        public void editTaskTitle(string boardName, string oldTitle, string newTitle)
+        public void editTaskTitle(Task task, string newTitle)
         {
-            throw new NotImplementedException();
+            if (!tasks.Contains(task))
+            {
+                throw new Exception("That task doesn't exist in this column");
+            }
+            task.editTaskTitle(newTitle);
         }
-        public void editTaskDescription(string boardName, string title, string newDescription)
+        public void editTaskDescription(Task task, string newDescription)
         {
-            throw new NotImplementedException();
+            if (!tasks.Contains(task))
+            {
+                throw new Exception("That task doesn't exist in this column");
+            }
+            task.editTaskDescription(newDescription);
         }
 
-        public void editTaskDueDate(string boardName, string title, DateTime newDueDate)
+        public void editTaskDueDate(Task task, DateTime newDueDate)
         {
-            throw new NotImplementedException();
-        }
-
-        internal Task GetTask(int taskId)
-        {
-            throw new NotImplementedException();
+            if (!tasks.Contains(task))
+            {
+                throw new Exception("That task doesn't exist in this column");
+            }
+            task.editTaskDueDate(newDueDate);
         }
     }
 }
