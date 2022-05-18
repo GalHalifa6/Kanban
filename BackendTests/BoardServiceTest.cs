a741995ff623f2d3007c917ca6d9ea75e23a49b6
Gal Halifa <halifag@bgu.ac.il>
2022-05-18 22:00:08 +0300

    check

--- BackendTests/BoardServiceTest.cs
+++ BackendTests/BoardServiceTest.cs
@@ -6,18 +6,14 @@ using System.Text.Json;
 
 namespace IntroSE.Kanban.Backend
 {
-
-
-
-    class BoardServiceTest
+    public class BoardServiceTest
     {
 
         public BoardServiceTest()
         {
 
         }
-
-        internal void runTests()
+        public void RunTests()
         {
             //addTaskTest();
             //removeBoardTest();
@@ -38,8 +34,7 @@ namespace IntroSE.Kanban.Backend
             gradingService.Login("gal@gmail.com", "123456Ab");
             gradingService.AddBoard("gal@gmail.com", "Board1");
             string res = gradingService.AddTask("gal@gmail.com", "Board1", "Task1", "Testing task1", new DateTime());
-            Console.WriteLine(res);
-            //Response res = JsonSerializer.Deserialize<Response>(jsonResponse);
+            Console.WriteLine(res);          
 
             Console.WriteLine("-----------------------");
             Console.WriteLine("Adding task to an non exist user. Should fail");
@@ -58,41 +53,27 @@ namespace IntroSE.Kanban.Backend
         */
 
 
-
+        /*
         ///<summary>
         ///This function test Requirement 9
         ///</summary>
-        public void removeTaskTest()
+        public void RemoveTaskTest()
         {
-            GradingService gradingService = new GradingService();
-            gradingService.Register("gal@gmail.com", "12345Aa");
-            gradingService.Login("gal@gmail.com", "12345Aa");
-
-
 
 
         }
+        */
 
 
 
-        /*
         ///<summary>
         ///This function test Requirement 14,15
         ///</summary>
         public void editTaskTest()
         {
-            BoardService board = new BoardService();
-            board.addTask("Task1", "Testing task1");
-            string jsonResponse = board.editTaskTitle("Task1", "Task2");
-            Response res = JsonSerializer.Deserialize<Response>(jsonResponse);
-            Console.WriteLine(res);
 
-            string jsonResponse1 = board.editTaskTitle("Task3", "Task4");
-            Response res1 = JsonSerializer.Deserialize<Response>(jsonResponse);
-            Console.WriteLine("The following test should failed:");
-            Console.WriteLine(res1);
         }
-        */
+
 
         /*
         ///<summary>
@@ -100,16 +81,7 @@ namespace IntroSE.Kanban.Backend
         ///</summary>
         public void editTaskDescriptionTest()
         {
-            BoardService board = new BoardService();
-            board.addTask("Task1", "Testing task1");
-            string jsonResponse = board.editTaskDescription("Task1", "Testing new");
-            Response res = JsonSerializer.Deserialize<Response>(jsonResponse);
-            Console.WriteLine(res);
-
-            string jsonResponse1 = board.editTaskDescription("Task3", "Testing New");
-            Response res1 = JsonSerializer.Deserialize<Response>(jsonResponse);
-            Console.WriteLine("The following test should failed:");
-            Console.WriteLine(res1);
+            
         }
         */
 
@@ -119,17 +91,7 @@ namespace IntroSE.Kanban.Backend
         ///</summary>
         public void editTaskDueDateTest()
         {
-            DateTime date = new DateTime();
-            BoardService board = new BoardService();
-            board.addTask("Task1", "Testing task1");
-            string jsonResponse = board.editTaskDueDate("Task1", date);
-            Response res = JsonSerializer.Deserialize<Response>(jsonResponse);
-            Console.WriteLine(res);
-
-            string jsonResponse1 = board.editTaskDueDate("Task3", date);
-            Response res1 = JsonSerializer.Deserialize<Response>(jsonResponse);
-            Console.WriteLine("The following test should failed:");
-            Console.WriteLine(res1);
+          
         }
         */
 
@@ -141,16 +103,7 @@ namespace IntroSE.Kanban.Backend
         ///</summary>
         public void addBoardTest()
         {
-            BoardService board = new BoardService();
-            string jsonResponse = board.addBoard("Board1");
-            Response res = JsonSerializer.Deserialize<Response>(jsonResponse);
-            Console.WriteLine(res);
-
-            string jsonResponse1 = board.addBoard("Board1");
-            Response res1 = JsonSerializer.Deserialize<Response>(jsonResponse);
-            Console.WriteLine("The following test should failed:");
-
-            Console.WriteLine(res1);
+            
         }
         */
 
@@ -192,29 +145,12 @@ namespace IntroSE.Kanban.Backend
         ///<summary>
         ///This function test Requirement 13
         ///</summary>
-        public void advanceTaskPhaseTest(string title)
+        public void advanceTaskPhaseTest(string Title)
         {
-            BoardService board = new BoardService();
-            board.addTask("Task1", "Testing task1");
-            string jsonResponse1 = board.advanceTask("Task1");
-            Response res = JsonSerializer.Deserialize<Response>(jsonResponse1);
-            Console.WriteLine(res);
-
-
-            string jsonResponse2 = board.advanceTask("Task2");
-            Console.WriteLine("The following test should failed:");
-            Response res2 = JsonSerializer.Deserialize<Response>(jsonResponse2);
-            Console.WriteLine(res2);
-
-            board.advanceTask("Task1");
-            string jsonResponse3 = board.advanceTask("Task1");
-            Response res3 = JsonSerializer.Deserialize<Response>(jsonResponse3);
-            Console.WriteLine("The following test should failed:");
-            Console.WriteLine(res3);
+          
 
 
         }
         */
-
     }
 }
