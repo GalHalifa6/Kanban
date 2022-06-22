420eb89fe9370726574fabf07e7c1cf270f5cd50
Gal Halifa <halifag@bgu.ac.il>
2022-06-22 14:22:06 +0300

    git

--- Backend/ServiceLayer/Tester.cs
+++ Backend/ServiceLayer/Tester.cs
@@ -13,6 +13,7 @@ namespace IntroSE.Kanban.Backend.ServiceLayer
         {
             DBConnector db = DBConnector.GetInstance();
             ServiceController sc = new ServiceController();
+            /*
           Console.WriteLine(sc.Register("itay@gmail.com", "Aa123456"));
             Console.WriteLine(sc.Login("itay@gmail.com", "Aa123456"));
             Console.WriteLine(sc.AddBoard("itay@gmail.com", "B1"));
@@ -36,6 +37,16 @@ namespace IntroSE.Kanban.Backend.ServiceLayer
             Console.WriteLine(sc.Login("tomer@gmail.com", "Aa123456"));
             Console.WriteLine(sc.GetUserBoards("tomer@gmail.com"));
            */
+
+            Console.WriteLine(sc.Register("itay@gmail.com", "Aa123456"));
+            Console.WriteLine(sc.Login("itay@gmail.com", "Aa123456"));
+            Console.WriteLine(sc.AddBoard("itay@gmail.com", "B1"));
+            Console.WriteLine(sc.Register("itay2@gmail.com", "Aa123456"));
+            Console.WriteLine(sc.Login("itay2@gmail.com", "Aa123456"));
+            Console.WriteLine(sc.AddBoard("itay2@gmail.com", "B1"));
+
+            Console.WriteLine(sc.JoinBoard("itay2@gmail.com", 0));
+
 
 
         }
